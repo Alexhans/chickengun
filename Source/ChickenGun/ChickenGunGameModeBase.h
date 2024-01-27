ac17bfbdcc1fba4e281c7c340851cbd1c2d40b80
Alex Hans <alexhans.dev@gmail.com>
2024-01-27 03:22:53 +0000

    Source and Config

---
+++ Source/ChickenGun/ChickenGunGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChickenGunGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHICKENGUN_API AChickenGunGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
